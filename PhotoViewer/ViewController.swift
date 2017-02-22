4e7589b9a55ed8b7058cee8965dca1d86ed641e0
Milovan Minic <milovanminic@Donalds.local>
2017-02-22 20:26:02 +0100

    Fetching photos to be viewed

--- PhotoViewer/ViewController.swift
+++ PhotoViewer/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // This is a picture to load!
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
